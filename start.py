ffa953c56d1a1f0ad1e32ffa901dcf3caf1f1ed3
ccc007 <1492949083@qq.com>
2023-10-07 11:06:52 +0800

    增加多选 E 选项

--- start.py
+++ start.py
@@ -5,29 +5,48 @@ import os
 
 pag.FAILSAFE = False
 
+
 # 定义一个函数，用于检测是否出现了正确的提示
 def check_correct():
     # 尝试在屏幕上找到correct.png图片的位置
     try:
-        x, y = pag.locateCenterOnScreen('correct.png',confidence=0.9)
+        x, y = pag.locateCenterOnScreen("correct.png", confidence=0.9)
         pag.moveTo(x, y)
         # 如果找到了，返回True
         return True
     # 如果没有找到，抛出异常
     except TypeError:
-        print('Correct not found')
+        print("Correct not found")
         # 返回False
         return False
+
 
 # 定义一个函数，用于随机点击一个选项
 def click_option():
     # 定义一个列表，存储所有选项的图片名
-    options = ['A.png', 'A_D.png','A_DV.png', 'B.png', 'B_D.png', 'B_DV.png', 'C.png', 'C_D.png', 'C_DV.png', 'D.png', 'D_D.png', 'D_DV.png']
+    options = [
+        "A.png",
+        "A_D.png",
+        "A_DV.png",
+        "B.png",
+        "B_D.png",
+        "B_DV.png",
+        "C.png",
+        "C_D.png",
+        "C_DV.png",
+        "D.png",
+        "D_D.png",
+        "D_DV.png",
+        "E_D.png",
+        "E_DV.png",
+    ]
     # 从列表中随机选择一个图片名
     option = random.choice(options)
     # 尝试在屏幕上找到该图片的位置
     try:
-        x, y = pag.locateCenterOnScreen(option,region=(616, 325, 1250, 810),grayscale=True,confidence=0.9)
+        x, y = pag.locateCenterOnScreen(
+            option, region=(616, 325, 1250, 810), grayscale=True, confidence=0.9
+        )
         # 如果找到了，移动鼠标到该位置，并点击一次
         pag.moveTo(x, y)
         pag.click()
@@ -35,13 +54,14 @@ def click_option():
     # 如果没有找到，抛出异常
     except TypeError:
         # 打印错误信息，并继续执行
-        print('未找到选项')
+        print("未找到选项")
+
 
 # 定义一个函数，用于点击关闭按钮
 def click_close():
     # 尝试在屏幕上找到close.png图片的位置
     try:
-        x, y = pag.locateCenterOnScreen('close.png')
+        x, y = pag.locateCenterOnScreen("close.png")
         # 如果找到了，移动鼠标到该位置，并点击一次
         pag.moveTo(x, y)
         pag.click()
@@ -50,13 +70,14 @@ def click_close():
     # 如果没有找到，抛出异常
     except TypeError:
         # 打印错误信息，并继续执行
-        print('Close button not found')
+        print("Close button not found")
+
 
 # 定义一个函数，用于点击后面的题目
-def click_next(int):#传入循环次数
+def click_next(int):  # 传入循环次数
     # 尝试在屏幕上找到循环次数.png图片的位置
     try:
-        x, y = pag.locateCenterOnScreen(str(int)+'.png')
+        x, y = pag.locateCenterOnScreen(str(int) + ".png")
         # 如果找到了，移动鼠标到该位置，并点击一次
         pag.moveTo(x, y)
         pag.click()
@@ -65,11 +86,15 @@ def click_next(int):#传入循环次数
     # 如果没有找到，抛出异常
     except TypeError:
         # 打印错误信息，并继续执行
-        print('Next button not found')
+        print("Next button not found")
         return False
     except OSError:
         # 打印错误信息，并继续执行
-        print('Failed to read ' + str(int) + '.png because file is missing, has improper permissions, or is an unsupported or invalid format')
+        print(
+            "Failed to read "
+            + str(int)
+            + ".png because file is missing, has improper permissions, or is an unsupported or invalid format"
+        )
         return False
 
 
@@ -79,7 +104,7 @@ while True:
     # 调用检测正确提示的函数，如果找到了，就调用点击关闭按钮的函数，并结束循环
     if check_correct():
         i += 1
-        #检查有没有下一题
+        # 检查有没有下一题
         if click_next(i):
             continue
         else:
